1e06b6f074ef51d07aa8b66b791f584e9927d042
Mitalikaushik <mits.kaushik22@gmail.com>
2022-08-04 12:22:18 +0530

    Added calculator.py script to repo

---
+++ calculator.py
@@ -0,0 +1,10 @@
+# This program does simple calculation
+
+# This function add two numbers
+def add(x,y):
+ return x + y
+
+a = input("enter your first number: ")
+b = input("enter your second number: ")
+
+print("the sum of two numbers is {}".format(add(a,b))
